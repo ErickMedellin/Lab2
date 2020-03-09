38f49725aad1156615632ab3bf2ccb5a97ae6114
Erick Jahir Medellin Camarillo <erickmedellincamarillo@tgmail.com>
2020-03-09 12:11:41 -0400

    Buscar

--- ScriptBD.sql
+++ ScriptBD.sql
@@ -25,4 +25,16 @@ BEGIN
 END
 EXECUTE sp_video_eliminar 1
 
+CREATE PROCEDURE sp_video_edit
+@idVideo int,
+@titulo varchar(50),
+@repro int,
+@url varchar(50)
+AS
+BEGIN
+	UPDATE video SET titulo=@titulo, repro=@repro, url=@url WHERE idVideo=@idVideo
+END
+
+execute sp_video_edit 1,'eminem',45000,'youtube.com'
+
 SELECT * FROM video;
